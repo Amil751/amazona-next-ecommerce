4e35a624e9974e19cba605e7dc37670fa2b9d990
Amil Rustamov <amil93.rustamov@gmail.com>
2022-05-27 17:10:33 +0400

    dffd

--- pages/cart.js
+++ pages/cart.js
@@ -1,20 +1,22 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Card } from "@mui/material";
 import React from "react";
 import Layout from "../components/Layout";
 
 export default function CartScreen() {
   return (
-    <div>
-      <Layout>
-        <Grid conatiner spacing={3}>
-                  <Grid item md={6} color='purple'>
-                     <Typography>amil</Typography>
-                  </Grid>
-                  <Grid item md={6}>
-                      <Typography>amilo</Typography>
-                  </Grid>
+    <Layout>
+      <Grid container spacing={2}>
+        <Grid item xs={8} color="purple">
+          <Card>
+            <Typography>amil</Typography>
+          </Card>
         </Grid>
-      </Layout>
-    </div>
+        <Grid item xs={4}>
+          <Card>
+            <Typography>amil</Typography>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
   );
 }
